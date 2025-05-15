e2030e96d7387ac0ca22f3e171a43d6d650395c0
AdrienHld <adrien.halard@edu.ece.fr>
2025-05-15 17:35:09 +0200

    Update niveau.h
    vitesse coeurs

--- niveau.h
+++ niveau.h
@@ -9,10 +9,18 @@
 #define VITESSE_ENNEMI_N2 6
 #define VITESSE_ENNEMI_N3 9
 
+// Vitesse coeurs par niveau
+#define VITESSE_COEUR_N1 3
+#define VITESSE_COEUR_N2 6
+#define VITESSE_COEUR_N3 9
+
 int demander_niveau(BITMAP *page, BITMAP *fond_menu);
 
 // Retourne la vitesse ennemis selon niveau
 int vitesse_ennemi_selon_niveau(int niveau);
+
+// Retourne la vitesse coeurs selon niveau
+int vitesse_coeur_selon_niveau(int niveau);
 
 #endif
 
