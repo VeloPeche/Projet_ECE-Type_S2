cc872029288152348d24cbe8d83b5d596f8602e1
AdrienHld <adrien.halard@edu.ece.fr>
2025-05-15 17:29:58 +0200

    Update donnees.h
    ajout coeur

--- donnees.h
+++ donnees.h
@@ -1,18 +1,28 @@
 #ifndef DONNEES_H
 #define DONNEES_H
 
-#include <allegro.h>
-
 #define MAX_PROJECTILES 10
+
 #define MAX_ENNEMIS    20
 #define LARGEUR_ENNEMI 80
 #define HAUTEUR_ENNEMI 74
 #define VITESSE_ENNEMI 5
+
+#define MAX_COEURS   5
+#define LARGEUR_COEUR 80
+#define HAUTEUR_COEUR 74
+#define VITESSE_COEUR 5
+
 #define VIES_INITIALES 3
+
 #define DUREE_NIVEAU   30
 
 typedef struct { int x, y, actif; } Projectile;
 typedef struct { int x, y, vitesse, largeur, hauteur, nb_vie; } Vaisseau;
 typedef struct { int x, y, actif; } Ennemi;
+typedef struct { int x, y, actif; } Coeur;
+
 
 #endif // DONNEES_H
+
+
